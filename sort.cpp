1316b5422ad11f92e2024e2b00736386b91ddcf4
Sean Chen <seachen4411@gmail.com>
2023-11-03 23:40:02 -0600

    Improved Readability of code

--- sort.cpp
+++ sort.cpp
@@ -1,80 +1,55 @@
 #include <iostream>
 #include <regex>
 
-#ifndef _WIN32
-#define ending true
-#endif
-#ifndef __linux__
-#define ending false
-#endif
+int main(int argc, char* argv[]) {
+    try {
+        if(argc > 1) {
+            throw std::runtime_error("No Arguments Plz");
+        }
+    }
+    catch(const std::exception& e) {
+        std::cerr << e.what() << '\n';
+        exit(0);
+    }
+    
+    int arr[11];
+    std::string text;
+    int max = 0;
+    std::regex p("[^0-9]");
 
-int main(int argc, char* argv[])
-{
-	try
-	{
-		if(argc > 1)
-		{
-			throw std::runtime_error("No Arguments Plz");
-		}
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-		exit(0);
-	}
-	
-	int arr[11];
-	std::string text;
-	int max = 0;
-	std::regex p("[^0-9]");
+    std::cout << "Please enter 10 integer values" << std::endl;
 
-	std::cout << "Please enter 10 integer values" << std::endl;
+    for (int i = 0; i < 10; i++) {
+        std::cin >> text;
+        if(regex_search(text, p)) {
+            std::cout << "Nope, try again" << std::endl;
+            i--;
+        }
+        else {
+            arr[i] = stoi(text);
+        }
+    }
 
-	for (int i = 0; i < 10; i++)
-	{
-		std::cin >> text;
-		if(regex_search(text, p))
-		{
-			std::cout << "Nope, try again" << std::endl;
-			i--;
-		}
-		else
-		{
-			arr[i] = stoi(text);
-		}
-	}
+    std::cout << "Executing Stalin Sort" << std::endl;
+    max = arr[0];
 
-	std::cout << "Executing Stalin Sort" << std::endl;
+    for (int i = 0; i < 10; i++) {
+        int j = i + 1;
 
-	max = arr[0];
+        if (max < arr[i]) {
+            max = arr[i];
+        }
 
-	for (int i = 0; i < 10; i++)
-	{
-		int j = i + 1;
+        if (max > arr[j]) {
+            arr[j] = 0;
+        }
+    }
 
-		if (max < arr[i])
-		{
-			max = arr[i];
-		}
+    for (int i = 0; i < 10; i++) {
+        if (arr[i] != 0) {
+            std::cout << arr[i] << std::endl;
+        }
+    }
 
-		if (max > arr[j])
-		{
-			arr[j] = 0;
-		}
-	}
-
-	for (int i = 0; i < 10; i++)
-	{
-		if (arr[i] != 0)
-		{
-			std::cout << arr[i] << std::endl;
-		}
-	}
-
-	if(ending)
-	{
-		system("pause");
-	}
-
-	return 0;
+    return 0;
 }
